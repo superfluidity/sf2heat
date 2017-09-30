cdbf8878c155d00926aaad6a2ebbe85a6af5567b
Francesco Lombardo <franclombardo@gmail.com>
2017-09-30 15:26:15 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sfheat',
+    version='0.1',
+    packages=['translator'],
+    url='',
+    license='',
+    author='Francesco Lombardo',
+    author_email='franclombardo@gmail.com',
+    description='SuperFluidity HEAt Transaltor'
+)
